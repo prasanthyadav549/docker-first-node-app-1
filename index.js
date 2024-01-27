e1dad0599cc6974d95c1175b2239ad85a0bd71da
prasanthyadav549 <prasanthyadav549@gmail.com>
2024-01-27 11:38:40 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+
 app.get("/home", (req, res) => {
   res.send({
     OK: "success",
